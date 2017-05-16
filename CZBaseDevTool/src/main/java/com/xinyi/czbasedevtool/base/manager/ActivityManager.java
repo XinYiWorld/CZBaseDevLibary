5ce894e5dc44451acd536c80899958900a438f75
XinYiWorld <1349308479@qq.com>
2017-05-16 10:35:47 +0800

    add a method for activitymanager

--- CZBaseDevTool/src/main/java/com/xinyi/czbasedevtool/base/manager/ActivityManager.java
+++ CZBaseDevTool/src/main/java/com/xinyi/czbasedevtool/base/manager/ActivityManager.java
@@ -97,6 +97,19 @@ public class ActivityManager {
     }
 
     /**
+     * 结束所有Activity,除了。
+     */
+    public void finishAllActivityExcept(Class<?> cls) {
+        for (int i = 0, size = activityStack.size(); i < size; i++) {
+            if (null != activityStack.get(i) && !activityStack.get(i).getClass().equals(cls)) {
+                finishActivity(activityStack.get(i));
+                break;
+            }
+        }
+        activityStack.clear();
+    }
+
+    /**
      * 获取指定的Activity
      *
      * @author kymjs
