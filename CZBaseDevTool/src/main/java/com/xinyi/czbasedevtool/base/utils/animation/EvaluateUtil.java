fca089294957c0e0b6b341dfd6d9ebf3e394cb44
XinYiWorld <1349308479@qq.com>
2017-06-13 11:27:13 +0800

    增加估值器

---
+++ CZBaseDevTool/src/main/java/com/xinyi/czbasedevtool/base/utils/animation/EvaluateUtil.java
@@ -0,0 +1,40 @@
+package com.xinyi.czbasedevtool.base.utils.animation;
+
+/**
+ * Created by 陈章 on 2017/6/13 0013.
+ * func: 估值器
+ */
+
+public class EvaluateUtil {
+    // 颜色过度器
+    public static Object evaluateColor(float fraction, Object startValue, Object endValue) {
+        int startInt = (Integer) startValue;
+        int startA = (startInt >> 24) & 0xff;
+        int startR = (startInt >> 16) & 0xff;
+        int startG = (startInt >> 8) & 0xff;
+        int startB = startInt & 0xff;
+
+        int endInt = (Integer) endValue;
+        int endA = (endInt >> 24) & 0xff;
+        int endR = (endInt >> 16) & 0xff;
+        int endG = (endInt >> 8) & 0xff;
+        int endB = endInt & 0xff;
+
+        return (int)((startA + (int)(fraction * (endA - startA))) << 24) |
+                (int)((startR + (int)(fraction * (endR - startR))) << 16) |
+                (int)((startG + (int)(fraction * (endG - startG))) << 8) |
+                (int)((startB + (int)(fraction * (endB - startB))));
+    }
+
+    /**
+     * TypeEvaluator 类型估值器.
+     * @param fraction
+     * @param startValue
+     * @param endValue
+     * @return
+     */
+    public static Float evaluate(float fraction, Number startValue, Number endValue) {
+        float startFloat = startValue.floatValue();
+        return startFloat + fraction * (endValue.floatValue() - startFloat);
+    }
+}
